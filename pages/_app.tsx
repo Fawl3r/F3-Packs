2b03a6246002febc0f2d11a16055323156c64628
Fawl3r <Fawl3r@gmail.com>
2023-10-07 18:00:19 -0500

    up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
-
+import { NETWORK } from "../const/addresses";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
@@ -10,11 +10,12 @@ const activeChain = "binance";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider clientId= {process.env.YOUR_CLIENT_ID}
+    activeChain={NETWORK}
+  >
       <Navbar />
       <Component {...pageProps} />
     </ThirdwebProvider>
-  );
-}
+  );}
 
 export default MyApp;
